d95fc2c45f4001d2eaf608a65bd996294995d687
Shreyash Pandey <93475758+Shreyash-Pandey90@users.noreply.github.com>
2023-06-26 09:20:32 +0530

    Lecture_5.java

---
+++ Lecture_5.java
@@ -0,0 +1,14 @@
+**Lecture_5 cipher school code about increment operator**
+
+public class Lecture5 
+{
+    public static void main(String[] args)
+    {
+    float a=3.5f; //bcz by default it is double
+    int m=5;
+    m =m++;
+    System.out.println(m);
+    }
+
+
+}
